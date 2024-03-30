37c3a0534e45518be6f3d9a41c1f89d811de7b0d
kendrick010 <nguyen.kendrick@yahoo.com>
2024-03-30 12:49:27 -0700

    open cv caracter detection

--- detect.py
+++ detect.py
@@ -4,21 +4,27 @@
 # screenshot_81 - babymario
 
 
-# next pos  screenshot_142
+# next pos  screenshot_109
 
 import cv2
 
 # Read an image
-image = cv2.imread('game_screenshots\screenshot_136.png')
+image = cv2.imread('game_screenshots\screenshot_109.png')
 
 # Define the coordinates of the region of interest (ROI)
 x, y, width, height = 5, 640, 50, 75  # Example coordinates (adjust as needed)
 
 head_crop = 40
+velocity_crop = 100
 
 # Draw a rectangle around the ROI on the original image
 cv2.rectangle(image, (x, y), (x + width, y + height), (0, 255, 0), 2)  # Green rectangle, thickness = 2
 cv2.rectangle(image, (x, y), (x + width, y + head_crop), (255, 0, 0), 2)  # Green rectangle, thickness = 2
+
+# Draw a rectangle around the ROI on the original image
+displaced_x = x + velocity_crop
+cv2.rectangle(image, (displaced_x, y), (displaced_x + width, y + height), (0, 255, 0), 2)  # Green rectangle, thickness = 2
+cv2.rectangle(image, (displaced_x, y), (displaced_x + width, y + head_crop), (255, 0, 0), 2)  # Green rectangle, thickness = 2
 
 # Crop the ROI from the image
 roi = image[y:y+height, x:x+width]
