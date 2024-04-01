14a2397a0e8847fcb0a178584090512094e8e0f9
kendrick010 <nguyen.kendrick@yahoo.com>
2024-04-01 13:13:59 -0700

    properties path

--- properties.py
+++ properties.py
@@ -1,4 +1,5 @@
 import json
+import os
 
 EMULATOR = "DeSmuME"
 
@@ -24,6 +25,9 @@ def load(filename="properties.json"):
     global UP_KEY, DOWN_KEY, RIGHT_KEY, LEFT_KEY
     global A_KEY, B_KEY, X_KEY, Y_KEY
     global RECORD_VIDEO
+    
+    local_dir = os.path.dirname(__file__)
+    filename = os.path.join(local_dir, filename)
     
     # I know it's messy
     with open(filename, "r") as property_file:
